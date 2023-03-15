e15c946b326c980c7e8b479b12703dcb49862ec1
Oat <oatt16@gmail.com>
2023-03-15 22:30:39 +0700

    Make register page

--- vue-contactList/src/views/Register.vue
+++ vue-contactList/src/views/Register.vue
@@ -1,13 +1,45 @@
 <template>
   <main>
     <div class="container">
-        <h1>Register</h1><br/>
+      <div class="loginPanel">
+        <h1>REGISTER</h1><br/>
+          <div>
+            <p class="info">USERNAME</p>
+            <input type="text" placeholder="" v-model="User.username"/>
+            <p class="info">PASSWORD</p>
+            <input type="password" placeholder="" v-model="User.password"/>
+          </div><br/>
+        <button type="submit" @click="addToAPI">REGISTER</button>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
-
+import axios from 'axios'
+export default {
+  name: 'AddUser',
+  data() {
+    return {
+      User: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    addToAPI() {
+      console.log(this.User)
+      axios.post('http://127.0.0.1:5001/users', this.User)
+      .then((response)=>{
+        console.log(response)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }
+  }
+}
 </script>
 
 <style>
