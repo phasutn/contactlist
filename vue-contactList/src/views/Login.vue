ab0a12785676e3a0069f720f30b986650eee6c19
Oat <oatt16@gmail.com>
2023-03-16 20:14:55 +0700

    Responsive Login Page for Empty/Incorrect Inputs

--- vue-contactList/src/views/Login.vue
+++ vue-contactList/src/views/Login.vue
@@ -6,10 +6,11 @@
           <div>
             <p class="info" >USERNAME</p>
             <input v-model="username" type="text" placeholder="Username">
-            <div id="username_error" class="input_error" >Invalid Username</div>
+            <div id="username_empty" class="input_error" >Invalid Username</div>
+            <div id="username_incorrect" class="input_error" >Incorrect username or password</div>
             <p class="info" placeholder="Password">PASSWORD</p>
             <input v-model="password" type="password" placeholder="Password">
-            <div id="password_error" class="input_error">Invalid Password</div>
+            <div id="password_empty" class="input_error">Invalid Password</div>
           </div><br/>
           <h5>NO ACCOUNT? <a href="/register">SIGN UP HERE</a></h5>          <br/>
         <button type="submit" @click="login">SIGN IN</button>
@@ -29,21 +30,30 @@ export default{
     }
   },
   methods: {
-    goToContact() {
-      this.$router.push('/contactlist');
-    },
+    
   async login() {
-      let user_error = document.getElementById('username_error')
-      let pass_error = document.getElementById('password_error')
-      if(this.username = ""){
-        user_error.style.display = "block"
-      }
-      if(this.password = ""){
-        pass_error.style.display = "block";
-      }
+      let user_empty = document.getElementById('username_empty')
+      let pass_empty = document.getElementById('password_empty')
+      let user_incorrect = document.getElementById('username_incorrect')
+
+      user_empty.style.display = (this.username.length == 0) ? "block" : "none";
+      pass_empty.style.display = (this.password.length == 0) ? "block" : "none";
+      
+      
       let result = axios.get(`http://localhost:5001/auser?username=${this.username}&password=${this.password}`)
       .then((result) => {
         console.log(result.data);
+        //each user/pass should be unique, thus 1
+        if(result.data.length == 1){
+          this.$router.push('/contactlist');
+        }
+        else{
+          if(user_empty.style.display == "none" && pass_empty.style.display == "none"){
+            user_incorrect.style.display = "block"
+          } else {
+            user_incorrect.style.display = "none"
+          }
+        }
       })
       .catch((error) => {
         console.error(error);
@@ -65,11 +75,20 @@ p.info {
   margin-top: 10%;
 }
 
-#username_error,
-#password_error{
+#username_empty,
+#password_empty{
   margin-left: 3px;
   opacity: 60%;
   font-size: 80%;
+  text-align: left;
+  color: red;
+  display: none;
+}
+
+#username_incorrect{
+  margin-left: 3px;
+  opacity: 60%;
+  font-size: 75%;
   text-align: left;
   color: red;
   display: none;
